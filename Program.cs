f7da5cc0b766c5374ca3c1a51747ab284b6475c8
Евгений Кучма <desferto87@gmail.com>
2024-12-10 14:30:22 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkCrystalStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int costOneCrystal = 11;
+            int userNumberGold;
+            int userNumberCrystal;
+            int userRisedueGold;
+
+            Console.Write("Hello traveler how mach gold you have?: ");
+            userNumberGold = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Ok, do you have {userNumberGold} gold, " +
+                $"remeber price per crystal {costOneCrystal}," +
+                $" now how mach crytal do you want by?: ");
+            userNumberCrystal = Convert.ToInt32(Console.ReadLine());
+
+            userRisedueGold = userNumberGold - userNumberCrystal * costOneCrystal;
+
+            Console.WriteLine($"Do you have {userNumberCrystal} crystal and you gold risedue {userRisedueGold}");
+        }
+    }
+}
